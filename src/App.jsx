3824f7c8d24604fa6f253bf95612882aa8b4f213
Arjun Balan <arjunbalan1994@gmail.com>
2024-04-19 16:21:09 +0530

    Starter files added and tailwind config done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <h1 className='bg-red-500 text-5xl text-white text-center'>Hello Tailwind</h1>
+    </>
+  )
+}
+
+export default App
